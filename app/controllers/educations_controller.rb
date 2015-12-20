2faa4729bffc9eea33f12649fd5fae2baae90ef1
nSchool <nschool@nickr-macbook.local>
2015-12-20 13:39:45 -0800

    Adding more to education controller

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -5,22 +5,44 @@ class EducationsController < ApplicationController
   end
 
   def create
-    @educations = Education.new( start_date: params[:start_date],
+    @education = Education.new( start_date: params[:start_date],
       end_date: params[:end_date], 
       degree: params[:degree],
       phone: params[:phone],
       university_name: params[:university_name],
       details: params[:details],
       student_id: params[:student_id])
-      if @educations.save
-        redirect_to "/students/#{@student.id}"
-      else
-        render json: ""
-      end
+    if @education.save
+      redirect_to "/students/#{@student.id}"
+    else
+      render json: @education.errors.full_messages
     end
-
-    def show
-      @educations = Education.find(params[:id])
-    end
-
   end
+
+  def update
+    @education = Student.find(params[:id])
+    @education.attributes(start_date: params[:start_date],
+      end_date: params[:end_date], 
+      degree: params[:degree],
+      phone: params[:phone],
+      university_name: params[:university_name],
+      details: params[:details],
+      student_id: params[:student_id)
+    if @education.save
+      redirect_to "/students/#{@student.id}"
+    else
+      render json: @education.errors.full_messages
+    end
+  end
+
+  def show
+    @education = Education.find(params[:id])
+  end
+
+  def destroy
+    @education = Education.find(params[:id])
+    @education.destroy
+    redirect_to "/students"
+  end
+
+end
