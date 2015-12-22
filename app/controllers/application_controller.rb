f33c3c90adb28bca455fd5a2f6e6897bb25c89d5
Matt Gainer <matgainer@gmail.com>
2015-12-21 19:39:52 -0800

    fixed the code for devise and json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  
-  config.to_prepare do
-    DeviseController.respond_to :html, :json
-  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
